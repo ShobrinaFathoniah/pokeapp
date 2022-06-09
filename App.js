4c323ae8c2c73ee77be3c45a72e8f4c18a9070f8
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-06-09 10:24:44 +0700

    add logo + splash screen

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import codePush from 'react-native-code-push';
+import SplashScreen from 'react-native-splash-screen';
 
 const codePushOptions = {
   checkFrequency: codePush.CheckFrequency.ON_APP_START,
 };
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View>
       <Text>App</Text>
