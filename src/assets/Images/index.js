fa3caf28fffcdb2cc01b17ceb96de31fc97ef57c
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-06-09 10:41:24 +0700

    Add assets => fonts + images

---
+++ src/assets/Images/index.js
@@ -0,0 +1,8 @@
+//<a href="https://www.flaticon.com/free-icons/pokemon" title="pokemon icons">Pokemon icons created by Those Icons - Flaticon</a>
+import RegisPic from './pokemon-trainer.png';
+import LoginPic from './pikachu.png';
+import NotFound from './avatar.png';
+import PokeBall from './pokeball.png';
+import PokeBallOrange from './gaming.png';
+
+export {RegisPic, LoginPic, PokeBall, PokeBallOrange, NotFound};
