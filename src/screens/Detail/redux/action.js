d67d286945f1eda98eedb7c75919acf753ea9712
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-06-11 12:35:13 +0700

    continue doing the detail + poke bag (not finish yet)

--- src/screens/Detail/redux/action.js
+++ src/screens/Detail/redux/action.js
@@ -1,7 +1,9 @@
 import {Alert} from 'react-native';
 import axios from 'axios';
 import {setIsLoading} from '../../../store/globalAction';
-import {SET_DETAIL_POKEMON} from './types';
+import {SET_CATCH_MONS, SET_DETAIL_POKEMON} from './types';
+import {myDb} from '../../../utils/db';
+import {setDataUser} from '../../../store/userAction';
 
 export const getDetailPokemon = urlDetail => async dispatch => {
   try {
@@ -20,9 +22,37 @@ export const getDetailPokemon = urlDetail => async dispatch => {
   }
 };
 
+export const saveCatchMons =
+  (userId, prevMonsData, monsData) => async dispatch => {
+    try {
+      dispatch(setIsLoading(true));
+
+      await myDb.ref(`users/${userId}`).update({
+        catchMons: [...prevMonsData, monsData],
+      });
+
+      const results = await myDb.ref(`users/${userId}`).once('value');
+      console.log(results);
+      if (results.val()) {
+        dispatch(setDataUser(results.val()));
+        dispatch(setIsLoading(false));
+      }
+    } catch (error) {
+      dispatch(setIsLoading(false));
+      Alert.alert('Pemberitahuan', error);
+    }
+  };
+
 export const setDetailPokemon = data => {
   return {
     type: SET_DETAIL_POKEMON,
     detailPokemon: data,
+  };
+};
+
+export const setCatchMons = data => {
+  return {
+    type: SET_CATCH_MONS,
+    catchMons: data,
   };
 };
--- src/screens/Detail/redux/types.js
+++ src/screens/Detail/redux/types.js
@@ -1,1 +1,2 @@
 export const SET_DETAIL_POKEMON = 'SET_DETAIL_POKEMON';
+export const SET_CATCH_MONS = 'SET_CATCH_MONS';
